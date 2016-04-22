2614bc3e3a58d5db1d32e63b965b9a925f57aec7
gbolo <george.bolo@gmail.com>
2016-04-22 11:25:03 -0400

    re-enable all sensu checks in base

--- environments/production/modules/profile/manifests/base.pp
+++ environments/production/modules/profile/manifests/base.pp
@@ -4,7 +4,7 @@ class profile::base {
   contain ::ntp
   contain ::ssh::server
   include ::sensu
-#  contain ::openvmtools
+  contain ::openvmtools
   contain ::monit
   contain ::epel
   contain ::sudo
@@ -14,28 +14,28 @@ class profile::base {
   include ::profile::firewall::monit
   include ::profile::monitcheck::ntpd
   include ::profile::monitcheck::crond
-#  include ::profile::monitcheck::sensu_client
-# include ::profile::monitcheck::puppet_agent
+  include ::profile::monitcheck::sensu_client
+  include ::profile::monitcheck::puppet_agent
   include ::profile::monitcheck::sshd
-#  include ::profile::monitcheck::vmtoolsd
-#  include ::profile::sensucheck::puppet_agent
-#  include ::profile::sensucheck::load
-#  include ::profile::sensucheck::cpu
-#  include ::profile::sensucheck::swap
-#  include ::profile::sensucheck::memory
-#  include ::profile::sensucheck::disk
-#  include ::profile::sensucheck::proc_crond
-#  include ::profile::sensucheck::proc_sshd
-#  include ::profile::sensucheck::proc_monit
-#  include ::profile::sensucheck::proc_vmtoolsd
-#  include ::profile::sensucheck::proc_ntpd
-#  include ::profile::sensucheck::ntp_external
-#  include ::profile::sensucheck::ntpd
+  include ::profile::monitcheck::vmtoolsd
+  include ::profile::sensucheck::puppet_agent
+  include ::profile::sensucheck::load
+  include ::profile::sensucheck::cpu
+  include ::profile::sensucheck::swap
+  include ::profile::sensucheck::memory
+  include ::profile::sensucheck::disk
+  include ::profile::sensucheck::proc_crond
+  include ::profile::sensucheck::proc_sshd
+  include ::profile::sensucheck::proc_monit
+  include ::profile::sensucheck::proc_vmtoolsd
+  include ::profile::sensucheck::proc_ntpd
+  include ::profile::sensucheck::ntp_external
+  include ::profile::sensucheck::ntpd
 
   # Ordering 
   Class['sudo'] -> 
   Class['epel'] -> 
-#  Class['openvmtools'] -> 
+  Class['openvmtools'] -> 
   Class['monit']
 
   Class['ssh::server'] ->
